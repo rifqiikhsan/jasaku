f5493dff24b2cbe95b2788ed75f0b593b15dac39
rifqiikhsan <rifqiikhsan45@gmail.com>
2026-07-04 16:51:13 +0700

    BE:Update naming

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,20 +15,21 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   const config = new DocumentBuilder()
-  .setTitle('JasaKu API')
-  .setDescription('API Documentation JasaKu')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .setContact(
-  'JasaKu Support',
-  'https://wa.me/6289629814773',
-  'rifqiikhsan45@gmail.com'
-)
-  .build();
+    .setTitle('JasaKu API')
+    .setDescription('API Documentation JasaKu')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .setContact(
+      'JasaKu Support',
+      'https://wa.me/6289629814773',
+      'rifqiikhsan45@gmail.com',
+    )
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
